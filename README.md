4028ed7b8d850b9b6a2d4f90128be6864060beed
qiujiahong <qiujiahongde@163.com>
2019-06-07 09:53:38 +0800

    init

---
+++ README.md
@@ -0,0 +1,21 @@
+#  行走力量
+
+
+
+
+
+[小程序获取步数参考资料](https://developers.weixin.qq.com/miniprogram/dev/api/open-api/werun/wx.getWeRunData.html)
+[APP获取步数参考资料](https://uniapp.dcloud.io/api/other/sport)
+[解密数据](https://developers.weixin.qq.com/miniprogram/dev/framework/open-ability/signature.html#method-cloud)
+
+* 需要注意的事项
+  * 微信接口具备能力获取最近30天内的步数，30天以外丢失；
+  * 用户必须已经关注了关注微信运动公众号，才能够获取；
+  * 目前获取到了密文数据，还需要在后台解密。
+
+* 与小程序通讯的后台只能是有备案的域名；需要如下资源
+	* 测试云服务器，2c, 4G内存,60G硬盘
+	* 云服务器需要固定IP
+	* 一个备案了的域名（或者二级域名），域名指向云服务器的固定IP。
+
+
---
+++ pages/index/index.vue
@@ -0,0 +1,96 @@
+<template>
+	<view class="content">
+		<image class="logo" src="/static/logo.png"></image>
+		<view>
+			<text class="title">{{title}}</text>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: 'Hello'
+			}
+		},
+		onLoad() {
+			console.log("onload")
+			var _this = this;
+			wx.login({
+			 success: function(res){
+			   var appid = "wx0006556d6bc1bcac";
+			   var secret = "97af146b32d0ebec3fab9d25e9eb6945";
+			   if(res.code){
+				 wx.request({
+				   url: 'https://api.weixin.qq.com/sns/jscode2session?appid=' + appid + '&secret=' + secret + '&js_code=' + res.code + '&grant_type=authorization_code',
+				   header: {
+					 'content-type': 'json'
+				   },
+				   success: function(res){
+					 var session_key = res.data.session_key;
+					 console.log(session_key);
+					 _this.getData(appid,session_key);
+				   }
+				 })
+			   }
+			 }
+		   })
+		},
+		methods: {
+			getData(appid, session_key) {
+				wx.getWeRunData({
+					success: function(res) {
+						console.log(res);
+						debugger
+						console.log("appid:" + appid + "session_key:" + session_key + "encryptedData:" + res.encryptedData + "iv:" + res.iv);
+						var encryptedData = res.encryptedData;
+						var iv = res.iv;
+						debugger
+						//使用解密工具，链接地址：
+						//https://codeload.github.com/gwjjeff/cryptojs/zip/master
+						var pc = new WXBizDataCrypt(appid, session_key);
+						console.log(pc);
+						debugger
+						var data = pc.decryptData(encryptedData, iv)
+						console.log(data)
+						wx.showModal({
+							title: '提示',
+							content: '已经获取数据了，好开心',
+							showCancel: false,
+							confirmText: '知道了'
+						})
+					},
+					fail: function(res) {
+						console.log(res);
+						debugger
+						wx.showModal({
+							title: '提示',
+							content: '开发者未开通微信运动，请关注“微信运动”公众号后重试',
+							showCancel: false,
+							confirmText: '知道了'
+						})
+					}
+				})
+			}
+		}
+	}
+</script>
+
+<style>
+	.content {
+		text-align: center;
+		height: 400upx;
+	}
+
+	.logo {
+		height: 200upx;
+		width: 200upx;
+		margin-top: 200upx;
+	}
+
+	.title {
+		font-size: 36upx;
+		color: #8f8f94;
+	}
+</style>
